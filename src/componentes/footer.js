ec6be1db273d25ca7b9c44c9090c0eab7a982ba3
Julia Alves <juliaa.brito22@gmail.com>
2024-05-11 00:27:15 -0300

    footer

--- src/componentes/footer.js
+++ src/componentes/footer.js
@@ -1,15 +1,28 @@
 import styles from './footer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faSquareGithub } from '@fortawesome/free-brands-svg-icons';
+import {Routes, Route, Link} from 'react-router-dom';
+import Contato from '../componentes/contato';
+import Projetos from '../componentes/projetos';
 
 export default function Footer() {
     return(
     <>
     <footer>
-        <section>
+        <section className={styles.redesSociais}>
             <h2>Redes Sociais</h2>
+            <FontAwesomeIcon icon={faLinkedin} className={`${styles.icon} ${styles.linkedin}`}/>
+            <FontAwesomeIcon icon={faSquareGithub} className={`${styles.icon} ${styles.github}`}/>
+
         </section>
-        <section>
-            <p>Entre em contato comigo</p>
-            <p>Meus projetos</p>
+        <section className={styles.siteMap}>
+        <Link to='/contato' className={styles.link}>Entre em contato comigo</Link>
+        <Link to='/projetos' className={styles.link}>Meus projetos</Link>
+
+        <Routes>
+            <Route path='/contato' element={<Contato/>}/>
+            <Route path='/contato' element={<Projetos/>}/>
+        </Routes>
         </section>
         <section>
             <p>Email: juliaa.brito22@gmail.com</p>
