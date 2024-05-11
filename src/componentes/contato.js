5bb683c6b16d19957c5cd622588860de7c9e4e12
Julia Alves <juliaa.brito22@gmail.com>
2024-05-11 20:50:36 -0300

    contato form

--- src/componentes/contato.js
+++ src/componentes/contato.js
@@ -1,11 +1,23 @@
+import styles from './contato.module.css';
+import Decoration from '../imagens/decoration.png';
 export default function Contato() {
     return (
       <>
-      <section>
-        <h2>CONTATO</h2>
-        <p>Teste</p>
-        
-      </section>
+        <section className={styles.contato}>
+          <div className={styles.decoracao}>
+          <img src={Decoration}/>
+          </div>
+
+
+            <form action="https://formsubmit.co/juliaa.brito22@gmail.com" method="POST">
+                <h2>Entre em contato comigo</h2>
+                <input type="text" name="nome" placeholder='Nome' required/>
+                <input type="email" name="email" placeholder='Email' required/>
+                <textarea name='mensagem' placeholder='Mensagem' required></textarea>
+                <button className='enviar' type="submit">Enviar</button>
+            </form>
+
+            </section>
       </>
     );
   }
