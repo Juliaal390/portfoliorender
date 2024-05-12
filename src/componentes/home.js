a29c06823dd45e54343e4bc1b38685a10291e079
Julia Alves <juliaa.brito22@gmail.com>
2024-05-12 16:01:06 -0300

    tippy

--- src/componentes/home.js
+++ src/componentes/home.js
@@ -3,11 +3,20 @@ import eu from '../imagens/minhafoto.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHtml5, faCss3Alt, faSquareJs, faReact, faWordpress, faGitAlt, faGithub, faFigma } from '@fortawesome/free-brands-svg-icons';
 import { faDatabase } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+import 'react-tippy/dist/tippy.css';
+import {
+  Tooltip,
+} from 'react-tippy';
+
+
+
 
 
 export default function Home() {
     return (
       <>
+
       <section className={styles.home}>
       <section className={styles.sobre}>
         <img src={eu}/>
@@ -25,15 +34,41 @@ export default function Home() {
       <section className={styles.habilidades}>
         <h2>Habilidades</h2>
         <div>
+        <Tooltip title="HTML" position="bottom">
         <FontAwesomeIcon icon={faHtml5} className={`${styles.icon} ${styles.html}`} />
+        </Tooltip>
+
+        <Tooltip title="CSS" position="bottom">
         <FontAwesomeIcon icon={faCss3Alt} className={`${styles.icon} ${styles.css}`} />
+        </Tooltip>
+
+        <Tooltip title="JavaScript" position="bottom">
         <FontAwesomeIcon icon={faSquareJs} className={`${styles.icon} ${styles.js}`} />
+        </Tooltip>
+
+        <Tooltip title="React JS" position="bottom">
         <FontAwesomeIcon icon={faReact} className={`${styles.icon} ${styles.react}`} />
+        </Tooltip>
+
+        <Tooltip title="Banco de dados" position="bottom">
         <FontAwesomeIcon icon={faDatabase} className={`${styles.icon} ${styles.database}`} />
+        </Tooltip>
+
+        <Tooltip title="WordPress" position="bottom">
         <FontAwesomeIcon icon={faWordpress} className={`${styles.icon} ${styles.wordpress}`} />
+        </Tooltip>
+
+        <Tooltip title="Figma" position="bottom">
         <FontAwesomeIcon icon={faFigma} className={`${styles.icon} ${styles.figma}`}/>
+        </Tooltip>
+
+        <Tooltip title="Git" position="bottom">
         <FontAwesomeIcon icon={faGitAlt} className={`${styles.icon} ${styles.git}`} />
+        </Tooltip>
+
+        <Tooltip title="GitHub" position="bottom">
         <FontAwesomeIcon icon={faGithub} className={`${styles.icon} ${styles.github}`} />
+        </Tooltip>
         </div>
       </section>
       </section>
