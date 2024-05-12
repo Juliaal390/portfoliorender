df0b4261a337d9eaa077969c266b1a98463276e4
Julia Alves <juliaa.brito22@gmail.com>
2024-05-12 19:21:47 -0300

    polimento

--- src/componentes/home.js
+++ src/componentes/home.js
@@ -5,13 +5,7 @@ import { faHtml5, faCss3Alt, faSquareJs, faReact, faWordpress, faGitAlt, faGithu
 import { faDatabase } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import 'react-tippy/dist/tippy.css';
-import {
- Tooltip,
-} from 'react-tippy';
-
-
-
-
+import {Tooltip} from 'react-tippy';
 
 export default function Home() {
     return (
@@ -23,50 +17,49 @@ export default function Home() {
         <div className={styles.sobreAside}>
           <h2>Quem sou eu?</h2>
           <p>
-          Sou entusiasta de desenvolvimento web, atualmente cursando Sistemas de Informação na Universidade do Estado de Minas Gerais (UEMG). Para mim, desenvolvimento é uma forma de arte. Estou sempre buscando oportunidades que permitam expandir meus conhecimentos como desenvolvedora front-end. 
-          Com experiência em HTML, CSS, JavaScript, React JS e WordPress, estou constantemente aprimorando minhas habilidades técnicas. Além disso, estou familiarizada com ferramentas essenciais como Git e GitHub, garantindo um trabalho eficiente e colaborativo.
+          Sou atualmente estudante de Sistemas de Informação da Universidade do Estado de Minas Gerais (UEMG). Apesar de ter iniciado a faculdade em Março de 2023, tenho contato com desenvolvimento desde 2021, ano em que iniciei o curso de programação da Evolute e me apaixonei definitivamente pela área. Além disso, fui estagiária de TI de Março a Maio de 2024, no Jornal Folha da Manhã.
           </p>
 
-          <button>Currículo</button>
+          <a href='' download><button>Currículo</button></a>
         </div>
       </section>
 
       <section className={styles.habilidades}>
         <h2>Habilidades</h2>
         <div>
-        <Tooltip title="HTML" position="bottom">
+        <Tooltip title="HTML" position="bottom" arrow="true" theme='transparent'>
         <FontAwesomeIcon icon={faHtml5} className={`${styles.icon} ${styles.html}`} />
         </Tooltip>
 
-        <Tooltip title="CSS" position="bottom">
+        <Tooltip title="CSS" position="bottom" arrow="true" theme='transparent'>
         <FontAwesomeIcon icon={faCss3Alt} className={`${styles.icon} ${styles.css}`} />
         </Tooltip>
 
-        <Tooltip title="JavaScript" position="bottom">
+        <Tooltip title="JavaScript" position="bottom" arrow="true" theme='transparent'>
         <FontAwesomeIcon icon={faSquareJs} className={`${styles.icon} ${styles.js}`} />
         </Tooltip>
 
-        <Tooltip title="React JS" position="bottom">
+        <Tooltip title="React JS" position="bottom" arrow="true" theme='transparent'>
         <FontAwesomeIcon icon={faReact} className={`${styles.icon} ${styles.react}`} />
         </Tooltip>
 
-        <Tooltip title="Banco de dados" position="bottom">
+        <Tooltip title="Banco de dados" position="bottom" arrow="true" theme='transparent'>
         <FontAwesomeIcon icon={faDatabase} className={`${styles.icon} ${styles.database}`} />
         </Tooltip>
 
-        <Tooltip title="WordPress" position="bottom">
+        <Tooltip title="WordPress" position="bottom" arrow="true" theme='transparent'>
         <FontAwesomeIcon icon={faWordpress} className={`${styles.icon} ${styles.wordpress}`} />
         </Tooltip>
 
-        <Tooltip title="Figma" position="bottom">
+        <Tooltip title="Figma" position="bottom" arrow="true" theme='transparent'>
         <FontAwesomeIcon icon={faFigma} className={`${styles.icon} ${styles.figma}`}/>
         </Tooltip>
 
-        <Tooltip title="Git" position="bottom">
+        <Tooltip title="Git" position="bottom" arrow="true" theme='transparent'>
         <FontAwesomeIcon icon={faGitAlt} className={`${styles.icon} ${styles.git}`} />
         </Tooltip>
 
-        <Tooltip title="GitHub" position="bottom">
+        <Tooltip title="GitHub" position="bottom" arrow="true" theme='transparent'>
         <FontAwesomeIcon icon={faGithub} className={`${styles.icon} ${styles.github}`} />
         </Tooltip>
         </div>
