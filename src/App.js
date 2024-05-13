c3ef68abf3efbd830ce8492751fb5d87e45f9a9a
Julia Alves <juliaa.brito22@gmail.com>
2024-05-13 08:29:44 -0300

    responsividade celular

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Footer from './componentes/footer';
 export default function App() {
   return (
     <>
+    <div className='tela'>
     <Header/>
     <Footer/>
+    </div>
     </>
   );
 }
--- src/context/linkagem.js
+++ src/context/linkagem.js
@@ -6,23 +6,37 @@ export const LinkagemProvider =({children})=>{
     const [pagina, setPagina] = useState('home');
     const [stack, setStack] = useState('front');
     const clickHome = useRef();
+    
 
     useEffect(()=>{
       clickHome.current.click();
     }, []);
   
     useEffect(() => {
+      const windowsize = window.innerWidth;
       const getSeta = document.getElementById('setaImg');
   
       switch (pagina) {
         case 'home':
+          if(windowsize>768){
           getSeta.style.left = '12.2ch';
+          }else{
+            getSeta.style.left = '6.2ch';
+          }
           break;
         case 'projetos':
+          if(windowsize>768){
           getSeta.style.left = '30ch';
+          }else{
+            getSeta.style.left = '15ch';
+          }
           break;
         default:
+          if(windowsize>768){
           getSeta.style.left = '49ch';
+          }else{
+            getSeta.style.left = '24ch';
+          }
           break;
       }
     }, [pagina]);
