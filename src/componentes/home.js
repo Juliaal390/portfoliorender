b69cdf9525a2c1356ad50bd5207ce44b70c9d57f
Julia Alves <juliaa.brito22@gmail.com>
2024-06-05 12:13:55 -0300

    att

--- src/componentes/home.js
+++ src/componentes/home.js
@@ -1,12 +1,13 @@
 import styles from './home.module.css';
 import eu from '../imagens/minhafoto.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHtml5, faCss3Alt, faSquareJs, faReact, faNode, faGitAlt, faGithub, faFigma } from '@fortawesome/free-brands-svg-icons';
-import { faDatabase } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import 'react-tippy/dist/tippy.css';
 import {Tooltip} from 'react-tippy';
 import pdf from '../imagens/julia.pdf';
+import {  SiHtml5, SiCss3, SiJavascript, SiReact, SiNextdotjs, SiGithub, SiTypescript } from "react-icons/si";
+import { FaDatabase, FaFigma, FaGitAlt } from "react-icons/fa6";
+
+
 
 export default function Home() {
     return (
@@ -18,7 +19,7 @@ export default function Home() {
         <div className={styles.sobreAside}>
           <h2>Quem sou eu?</h2>
           <p>
-          Sou atualmente estudante de Sistemas de Informação da Universidade do Estado de Minas Gerais (UEMG). Apesar de ter iniciado a faculdade em Março de 2023, tenho contato direto com desenvolvimento desde 2021, ano em que iniciei o curso de programação da Evolute e me apaixonei definitivamente pela área. Estudo constantemente, e busco oportunidades de emprego ou estágio na área de tecnologia.
+          Sou atualmente graduanda de Sistemas de Informação da Universidade do Estado de Minas Gerais (UEMG). Apesar de ter iniciado a faculdade em Março de 2023, tenho contato direto com desenvolvimento desde 2021, ano em que iniciei o curso de programação da Evolute e me apaixonei definitivamente pela área. Estudo constantemente, e busco oportunidades de emprego ou estágio na área de tecnologia.
           </p>
 
           <a href={pdf} download='Julia Alves CV'><button>Currículo</button></a>
@@ -29,40 +30,44 @@ export default function Home() {
         <h2>Habilidades</h2>
         <div>
         <Tooltip title="HTML" position="bottom" arrow="true" theme='transparent'>
-        <FontAwesomeIcon icon={faHtml5} className={`${styles.icon} ${styles.html}`} />
+        <SiHtml5 className={`${styles.icon} ${styles.html}`} />
         </Tooltip>
 
         <Tooltip title="CSS" position="bottom" arrow="true" theme='transparent'>
-        <FontAwesomeIcon icon={faCss3Alt} className={`${styles.icon} ${styles.css}`} />
+        <SiCss3 className={`${styles.icon} ${styles.css}`} />
         </Tooltip>
 
         <Tooltip title="JavaScript" position="bottom" arrow="true" theme='transparent'>
-        <FontAwesomeIcon icon={faSquareJs} className={`${styles.icon} ${styles.js}`} />
+        <SiJavascript className={`${styles.icon} ${styles.js}`} />
+        </Tooltip>
+
+        <Tooltip title="TypeScript" position="bottom" arrow="true" theme='transparent'>
+        <SiTypescript className={`${styles.icon} ${styles.ts}`} />
         </Tooltip>
 
         <Tooltip title="React JS" position="bottom" arrow="true" theme='transparent'>
-        <FontAwesomeIcon icon={faReact} className={`${styles.icon} ${styles.react}`} />
+        <SiReact className={`${styles.icon} ${styles.react}`} />
         </Tooltip>
 
         <Tooltip title="Banco de dados" position="bottom" arrow="true" theme='transparent'>
-        <FontAwesomeIcon icon={faDatabase} className={`${styles.icon} ${styles.database}`} />
+        <FaDatabase className={`${styles.icon} ${styles.database}`} />
         </Tooltip>
 
 
         <Tooltip title="Figma" position="bottom" arrow="true" theme='transparent'>
-        <FontAwesomeIcon icon={faFigma} className={`${styles.icon} ${styles.figma}`}/>
+        <FaFigma className={`${styles.icon} ${styles.figma}`}/>
         </Tooltip>
 
-        <Tooltip title="Node JS" position="bottom" arrow="true" theme='transparent'>
-        <FontAwesomeIcon icon={faNode} className={`${styles.icon} ${styles.node}`}/>
+        <Tooltip title="Next JS" position="bottom" arrow="true" theme='transparent'>
+        <SiNextdotjs className={`${styles.icon} ${styles.next}`}/>
         </Tooltip>
 
         <Tooltip title="Git" position="bottom" arrow="true" theme='transparent'>
-        <FontAwesomeIcon icon={faGitAlt} className={`${styles.icon} ${styles.git}`} />
+        <FaGitAlt className={`${styles.icon} ${styles.git}`} />
         </Tooltip>
 
         <Tooltip title="GitHub" position="bottom" arrow="true" theme='transparent'>
-        <FontAwesomeIcon icon={faGithub} className={`${styles.icon} ${styles.github}`} />
+        <SiGithub className={`${styles.icon} ${styles.github}`} />
         </Tooltip>
 
         
